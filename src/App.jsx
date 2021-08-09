6be2b7e3d1e0618fa2cb9e76bb77bfea2831b45b
Shashank Zade <shashankzade07@gmail.com>
2021-08-09 11:10:14 +0530

    Added entertainment images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Route } from "react-router-dom";
-import Navbar from "./components/Navbar/navbar.component";
-import Temp from "./components/Temp";
+
+// HOC
 import DefaultHOC from "./HOC/Default.HOC";
 
+// Components
+import Temp from "./components/temp";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
