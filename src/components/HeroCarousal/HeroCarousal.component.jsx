51627038aeb251d664f5d453ed31afe78de75f4c
Shashank Zade <shashankzade07@gmail.com>
2021-08-09 11:01:32 +0530

    Added Hero carousel

--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -6,10 +6,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const HeroCarousal = () => {
+  const settingsLG = {
+    arrows: true,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: "300px",
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+  };
+
   const settings = {
     arrows: true,
-    centerMode: true,
-    centerPadding: "200px",
     dots: true,
     infinite: true,
     speed: 500,
@@ -18,23 +26,38 @@ const HeroCarousal = () => {
   };
 
   const images = [
-    "https://in.bmscdn.com/content-buzz/2021/02/buzz-stream-01.jpg",
-    "https://assets.mspimages.in/wp-content/uploads/2021/02/EtcKZdLUUAExdSF-696x365.jpg",
-    "https://in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-sea-wall-3-2021-3-11-t-19-25-57.jpg",
-    "https://pbmobi.payback.in/content/dam/payback/home/herobanner/Godzilla-Kingkong_banner_767x412.webp/jcr:content/renditions/original",
-    "https://pbs.twimg.com/media/DtEOqhxVYAAqrxg.jpg",
-    "https://www.marketingmind.in/wp-content/uploads/2019/03/BookMySHow.jpg",
+    "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1626128665085-483747621778?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=610&q=80",
+    "https://images.unsplash.com/photo-1622495894321-010d1e6db2a9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2775&q=80",
+    "https://images.unsplash.com/photo-1626050954744-92bf034ce476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+    "https://images.unsplash.com/photo-1626074961596-cab914d9392e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80",
   ];
 
   return (
     <>
-      <HeroSlider {...settings}>
-        {images.map((image) => (
-          <div className="w-20 h-96">
-            <img src={image} alt="testing" className="w-full h-full" />
-          </div>
-        ))}
-      </HeroSlider>
+      <div className="lg:hidden">
+        <HeroSlider {...settings}>
+          {images.map((image) => (
+            <div className="w-full h-56 md:h-80 py-3 ">
+              <img src={image} alt="testing" className="w-full h-full" />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
+
+      <div className="hidden lg:block">
+        <HeroSlider {...settingsLG}>
+          {images.map((image) => (
+            <div className="w-full h-96 px-2 py-3">
+              <img
+                src={image}
+                alt="testing"
+                className="w-full h-full rounded-md"
+              />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
     </>
   );
 };
